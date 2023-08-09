f3b6ee8c72d07b1c111a14cdab6a2558922ddb0f
Ann Likowo Molinge <ann.molinge@gmail.com>
2023-08-09 16:22:43 -0500

    added image to face of clock. Up date css

--- script.js
+++ script.js
@@ -5,22 +5,22 @@ const hourHand = document.querySelector('.hourHand')
 
 // Create fxn called setDate
 const setDate = () => {
-    const now = new Date();
+    const date = new Date();
 
-   const seconds = now.getSeconds();
+   const seconds = date.getSeconds();
 //    console.log(seconds)
    const secondsDegrees = ((seconds / 60) * 360) + 90; // Match seconds to degrees so seconds hand circles 360 degrees at 60 seconds
    secondsHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
-   const minutes = now.getMinutes();
+   const minutes = date.getMinutes();
 //    console.log(minutes)
-   const minutesDegrees = ((minutes / 60) * 360) + 90; // Match minutes to degrees so seconds hand  circles 360 degrees at 60 seconds
+   const minutesDegrees = ((minutes / 60) * 360) + 90; // Match minutes to degrees so minutes hand  moves to next number at 60 seconds
    minutesHand.style.transform = `rotate(${minutesDegrees}deg)`;
 
-   const hour = now.getMinutes(); //Set function as getMinutes, as 60 minutes make an hour
+   const hour = date.getMinutes(); //Set function as getMinutes, as 60 minutes make an hour
 //    console.log(hour)
-   const hourDegrees = ((minutes / 12) * 360) + 90; // Match minutes to degrees so seconds hand  circles 360 degrees at 60 seconds
-   hourHand.style.transform = `rotate(${hourDegrees}deg)`; 
-}
+   const hourDegrees = ((minutes / 12) * 360) + 90; // Match hour  to degrees so hour hand  circles 360 degrees at 60 minutes
+   hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+  }
 
-setInterval(setDate,1000); // 1000 is milliseconds that makes 1 minute
+setInterval(setDate,1000); //  Use setInverval method to Enables clock hands to run every second. 1000 is milliseconds that makes 1 minute
