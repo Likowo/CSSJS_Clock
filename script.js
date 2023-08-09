6b994148ad87dd33d5aeb01bb0ebbff4ac44616a
Ann Likowo Molinge <ann.molinge@gmail.com>
2023-08-09 13:03:53 -0500

    placed background image.Updated css

--- script.js
+++ script.js
@@ -9,19 +9,18 @@ const setDate = () => {
 
    const seconds = now.getSeconds();
 //    console.log(seconds)
-   const secondsDegrees = ((seconds / 60) * 360) + 90; // Match seconds to degrees so seconds hand is circles 360 degrees at 60 seconds
+   const secondsDegrees = ((seconds / 60) * 360) + 90; // Match seconds to degrees so seconds hand circles 360 degrees at 60 seconds
    secondsHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
    const minutes = now.getMinutes();
-   console.log(minutes)
+//    console.log(minutes)
    const minutesDegrees = ((minutes / 60) * 360) + 90; // Match minutes to degrees so seconds hand  circles 360 degrees at 60 seconds
    minutesHand.style.transform = `rotate(${minutesDegrees}deg)`;
 
-   const hour = now.gethour();
-   console.log(hour)
-   const hourDegrees = ((hour / 60) * 360) + 90; // Match minutes to degrees so seconds hand  circles 360 degrees at 60 seconds
-   hourHand.style.transform = `rotate(${hourDegrees}deg)`;
-   
+   const hour = now.getMinutes(); //Set function as getMinutes, as 60 minutes make an hour
+//    console.log(hour)
+   const hourDegrees = ((minutes / 12) * 360) + 90; // Match minutes to degrees so seconds hand  circles 360 degrees at 60 seconds
+   hourHand.style.transform = `rotate(${hourDegrees}deg)`; 
 }
 
-setInterval(setDate,1000) // 1000 is milliseconds that make 1 minute
+setInterval(setDate,1000); // 1000 is milliseconds that makes 1 minute
